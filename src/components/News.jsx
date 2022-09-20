1a6e2a445bccdfad8ed6c5df1e27acde7a413f5f
Ibrahim Sweidan <isweidan04@gmail.com>
2022-09-20 00:29:10 -0400

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -9,14 +9,32 @@ function news() {
             <h1>Newsletter</h1>
         </div>
         <hr/>
-        <h2>01: Welcome to EPAI!</h2>
-        <a target="_blank" rel="noopener noreferrer" href="https://mailchi.mp/3c5002594c76/epai-newsletter-sep-week-5092574?e=f4bc157416"><div class="text-center"><button style={{width:"100%"}} type="button" class="btn btn-info">Read Issue 01</button></div></a>
+
+      <p class="lead">The EPAI newsletter  includes short discussions of academic papers, news stories, and conferences to prompt discussion and keep members up-to-date on salient issues in Ethical AI.
+    </p>
+    <p class="lead">Have you found an interesting news article, research paper, or video on AI that you think others should know about? If you would like to submit a topic for discussion or write for the newsletter, please message Adele (adelell#6804) on Discord! </p>
+
+
+        <h2>04: EPAI Events and Leadership Applications</h2>
+        <a target="_blank" rel="noopener noreferrer" href="https://us5.campaign-archive.com/?e=__test_email__&u=cd0bced23d8762ad5ea829c18&id=af7d30a378"><div class="text-center"><button style={{width:"100%"}} type="button" class="btn btn-info">Read Issue 04</button></div></a>
         <ul>
-            <li>Upcoming events</li>
-            <li>How to join EPAI</li>
-            <li>Ethical AI resources</li>
-            <li>Meme of the Week</li>
+            <li>EPAI Speaker Event: Professor Ahmed on Ethical Pluralism and Decolonizing AI</li>
+            <li>Volunteer for the Hart House Show Debate</li>
+            <li>New EPAI Podcast: Modern Data</li>
+            <li>News in Ethical AI</li>
         </ul>
+
+        <h2>03: EPAI Events and Leadership Applications</h2>
+        <a target="_blank" rel="noopener noreferrer" href="https://us5.campaign-archive.com/?e=__test_email__&u=cd0bced23d8762ad5ea829c18&id=307ce072cf"><div class="text-center"><button style={{width:"100%"}} type="button" class="btn btn-info">Read Issue 03</button></div></a>
+        <ul>
+            <li>Volunteer for the Hart House Show Debate</li>
+            <li>Merch</li>
+            <li>Positions open for Hackathon Committee</li>
+            <li>EPAI Paper- Reading Group</li>
+        </ul>
+
+
+
 
         <h2>02: EPAI Events and Leadership Applications</h2>
         <a target="_blank" rel="noopener noreferrer" href="https://mailchi.mp/2783c94007bd/epai-newsletter-2-upcoming-workshop-info-session-recording-and-leadership-applications?e=396dbd5d52"><div class="text-center"><button style={{width:"100%"}} type="button" class="btn btn-info">Read Issue 02</button></div></a>
@@ -27,22 +45,13 @@ function news() {
             <li>Leadership Opportunities</li>
         </ul>
 
-        <h2>04: EPAI Events and Leadership Applications</h2>
-        <a target="_blank" rel="noopener noreferrer" href="https://us5.campaign-archive.com/?e=__test_email__&u=cd0bced23d8762ad5ea829c18&id=307ce072cf"><div class="text-center"><button style={{width:"100%"}} type="button" class="btn btn-info">Read Issue 04</button></div></a>
+        <h2>01: Welcome to EPAI!</h2>
+        <a target="_blank" rel="noopener noreferrer" href="https://mailchi.mp/3c5002594c76/epai-newsletter-sep-week-5092574?e=f4bc157416"><div class="text-center"><button style={{width:"100%"}} type="button" class="btn btn-info">Read Issue 01</button></div></a>
         <ul>
-            <li>Volunteer for the Hart House Show Debate</li>
-            <li>Merch</li>
-            <li>Positions open for Hackathon Committee</li>
-            <li>EPAI Paper- Reading Group</li>
-        </ul>
-
-        <h2>05: EPAI Events and Leadership Applications</h2>
-        <a target="_blank" rel="noopener noreferrer" href="https://us5.campaign-archive.com/?e=__test_email__&u=cd0bced23d8762ad5ea829c18&id=af7d30a378"><div class="text-center"><button style={{width:"100%"}} type="button" class="btn btn-info">Read Issue 05</button></div></a>
-        <ul>
-            <li>EPAI Speaker Event: Professor Ahmed on Ethical Pluralism and Decolonizing AI</li>
-            <li>Volunteer for the Hart House Show Debate</li>
-            <li>New EPAI Podcast: Modern Data</li>
-            <li>News in Ethical AI</li>
+            <li>Upcoming events</li>
+            <li>How to join EPAI</li>
+            <li>Ethical AI resources</li>
+            <li>Meme of the Week</li>
         </ul>
 
         <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLScBhjIogOTjRyTTOWOi_RD3iX1rcsrCKX9mffZjoloQ8v6--w/viewform"><p style={{color: "#FF6B48", "text-align": 'center', 'font-size':'20pt'}}><u>Subscribe to EPAI's Newsletter</u></p></a>
